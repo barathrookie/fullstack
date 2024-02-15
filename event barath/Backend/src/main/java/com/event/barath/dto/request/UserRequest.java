dafda168be4b076c7d9c46af9de5813800a812aa
barathrookie <113581068+barathrookie@users.noreply.github.com>
2024-02-15 12:31:17 +0530

    Add files via upload

---
+++ event barath/Backend/src/main/java/com/event/barath/dto/request/UserRequest.java
@@ -0,0 +1,17 @@
+package com.event.barath.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+    private String id;
+    private String name;
+    private String email;
+    
+}
